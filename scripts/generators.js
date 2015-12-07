bf21d7f26187ae1ad573fc5788f2ecd792e91eb3
Christopher Van <cvan@mozilla.com>
2015-12-07 10:08:27 -0800

    make Examples the Homepage

--- scripts/generators.js
+++ scripts/generators.js
@@ -4,6 +4,14 @@ hexo.extend.generator.register('post', function (locals) {
   var self = this;
   if (!locals.data.examples) { return; }
   var routes = [];
+  function addRoute (path, data) {
+    routes.push({
+      path: path,
+      data: data,
+      layout: 'example'
+    });
+  }
+  var examples = {};
   Object.keys(locals.data.examples).map(function (sectionSlug) {
     var section = locals.data.examples[sectionSlug];
     section.examples.forEach(function (example) {
@@ -11,19 +19,19 @@ hexo.extend.generator.register('post', function (locals) {
       example.type = 'examples';
       example.section = sectionSlug;
       example.url = permalink;
-      routes.push({
-        path: permalink,
-        data: example,
-        layout: 'example'
-      });
+      addRoute(permalink, example);
+      examples[permalink] = example;
+      if (!self.config.examples) { return; }
       if (permalink === self.config.examples.first_example_url) {
-        routes.push({
-          path: 'examples/',
-          data: example,
-          layout: 'example'
-        });
+        addRoute('examples/', example);
+      }
+      if (permalink === self.config.examples.homepage_example_url) {
+        addRoute('/', example);
       }
     });
+  });
+  hexo.locals.set('examples_by_urls', function () {
+    return examples;
   });
   return routes;
 });
