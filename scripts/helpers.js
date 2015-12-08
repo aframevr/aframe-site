ba225c246b77232449e612df8ab6657d99a32922
Christopher Van <cvan@mozilla.com>
2015-12-08 17:08:36 -0500

    support external URLs in examples :)

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,5 +1,9 @@
 var utils = require('../lib/utils');
 
+hexo.extend.helper.register('external_example_url', function (path) {
+  return utils.isUrl(path) ? path : utils.urljoin(this.config.examples.base_url, path);
+});
+
 hexo.extend.helper.register('github_file_url', function (path) {
   return this.config.github_url + '/blob/master/' + path;
 });
