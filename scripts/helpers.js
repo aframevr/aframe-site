ec39b3fc541e44e4fffb21d4bac2a63392abfb32
Christopher Van <cvan@mozilla.com>
2015-12-08 12:15:07 -0500

    clean up footers, add index blurb on homepage footer

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,6 +1,6 @@
 var utils = require('../lib/utils');
 
-hexo.extend.helper.register('github_file', function (path) {
+hexo.extend.helper.register('github_file_url', function (path) {
   return this.config.github_url + '/blob/master/' + path;
 });
 
