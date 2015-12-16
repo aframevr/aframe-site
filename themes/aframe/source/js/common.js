541ae7b35df51a47ddf1aa4179f85a18e61410ae
Christopher Van <cvan@mozilla.com>
2015-12-15 21:46:36 -0800

    check for existence of docs for heading jumps

--- themes/aframe/source/js/common.js
+++ themes/aframe/source/js/common.js
@@ -124,11 +124,14 @@
 
   var anchorHeadingsSelector = 'h2[id], h3[id], h4[id], h5[id], h6[id]';
 
-  $('.content--docs').addEventListener('click', function (e) {
-    var el = e.target;
-    if (el.matches && el.matches(anchorHeadingsSelector)) {
-      window.location.hash = '#' + el.id;
-    }
-  });
+  var contentDocs = $('.content--docs');
+  if (contentDocs) {
+    contentDocs.addEventListener('click', function (e) {
+      var el = e.target;
+      if (el.matches && el.matches(anchorHeadingsSelector)) {
+        window.location.hash = '#' + el.id;
+      }
+    });
+  }
 
 })();
