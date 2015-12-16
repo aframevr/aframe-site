85a288d99ba42fb7c7c90cd958d645cdfb140130
Christopher Van <cvan@mozilla.com>
2015-12-15 20:04:36 -0800

    enhance github config constants

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -9,15 +9,15 @@ hexo.extend.helper.register('external_example_url', function (path) {
 
 hexo.extend.helper.register('github_release_url', function (version) {
   version = version || ('v' + this.config.aframe_version);
-  return urljoin(this.config.github_url, 'releases', 'tag', version);
+  return urljoin(this.config.github.aframe.url, 'releases', 'tag', version);
 });
 
 hexo.extend.helper.register('github_file_url', function (path) {
-  return urljoin(this.config.github_url, 'blob', 'master', path);
+  return urljoin(this.config.github.aframe.url, 'blob', 'master', path);
 });
 
 hexo.extend.helper.register('website_github_file_url', function (path) {
-  return urljoin(this.config.website_github_url, 'blob', 'master', this.config.source_dir, path.replace(/\.html$/, '.md'));
+  return urljoin(this.config.github.aframe_site.url, 'blob', 'master', this.config.source_dir, path.replace(/\.html$/, '.md'));
 });
 
 hexo.extend.helper.register('page_url', function (path, options) {
