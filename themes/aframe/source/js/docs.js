5b44aea1679bd578a3c66d363568531b9267fdf2
Kevin Ngo <me@ngokevin.com>
2016-05-27 18:17:30 -0700

    lowercase all anchors

--- themes/aframe/source/js/docs.js
+++ themes/aframe/source/js/docs.js
@@ -1,11 +1,15 @@
 /* global $ */
-
 (function () {
 
 var anchorHeadingsSelector = 'h2[id], h3[id], h4[id], h5[id], h6[id]';
 
 var content = $('.content');
+
 if (content) {
+  if (window.location.hash) {
+    window.location.hash = window.location.hash.toLowerCase();
+  }
+
   content.addEventListener('click', function (e) {
     var el = e.target;
     if (el.matches && el.matches(anchorHeadingsSelector)) {
