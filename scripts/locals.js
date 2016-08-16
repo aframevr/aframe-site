ef1af651f4598fa751ffbd3c3a418dbb1d083727
Christopher Van <cvan@mozilla.com>
2016-08-15 20:40:29 -0700

    introduce local config file; fix broken URLs in examples; simplify examples
    * introduce local config YAML file (to point to aframevr/aframe repo examples, for example)
    * fix several broken URLs (paths in aframevr/aframe's `examples/` changed)
    * create filter to change base URLs for site, repo, and examples
    * disable SPA JS logic when boolean disabled
    * have server render example logic & HTML
    * simplify JS for Examples

---
+++ scripts/locals.js
@@ -0,0 +1,33 @@
+var url_for = hexo.extend.helper.store.url_for.bind(hexo);
+var utils = require('../lib/utils');
+
+function exampleUrlFor (item) {
+  return utils.urljoin(url_for('examples'), item.section, item.slug) + '/';
+}
+
+var transforms = {};
+
+transforms.examples = function (doc) {
+  doc.showcase = doc.showcase.map(function (example) {
+    if (!example.supports) {
+      example.supports = {};
+    }
+    if (typeof example.supports.android === 'undefined') {
+      example.supports.android = true;
+    }
+    if (typeof example.supports.ios === 'undefined') {
+      example.supports.ios = true;
+    }
+    example.example_url = exampleUrlFor(example);
+    return example;
+  });
+  return doc;
+};
+
+hexo.locals.set('data', function () {
+  var obj = {};
+  hexo.model('Data').forEach(function (doc) {
+    obj[doc._id] = doc._id in transforms ? transforms[doc._id](doc.data) : doc.data;
+  });
+  return obj;
+});
