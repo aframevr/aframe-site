834f79b95c4ed0408e6e05acb13ea924bb03a6fc
Patrick <patrick.arrow206@outlook.com>
2016-10-19 10:51:36 +0000

    Add Service Worker to Site

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+gulp.task('generate-service-worker', function(callback) {
+  var path = require('path');
+  var swPrecache = require('sw-precache');
+  var rootDir = 'scripts';
+
+  swPrecache.write(path.join(rootDir, 'sw.js'), {
+    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif}'],
+    stripPrefix: rootDir
+  }, callback);
+});
