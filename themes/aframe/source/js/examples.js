cff517f2dba27e92aa8810156d322aa639e37699
Kevin Ngo <me@ngokevin.com>
2016-10-26 02:20:22 -0700

    fix non-mobile examples showing up on mobile

--- themes/aframe/source/js/examples.js
+++ themes/aframe/source/js/examples.js
@@ -1,199 +1,74 @@
-/* global $, getJSON, settings, singlePage */
+/* global $ */
 (function () {
-
-var path = window.location.pathname;
-var pathWithSlash = forceTrailingSlash(path);
-
-var a = document.createElement('a');
-function parseUrl (url) {
-  a.href = url;
-  return a;
-}
-
-function stripTrailingSlash (url) {
-  return url.replace(/\/+$/, '');
-}
-
-function forceTrailingSlash (str) {
-  if (str.substr(-1) !== '/') {
-    str += '/';
+// Remove non-mobile examples on mobile.
+if (document.documentElement.getAttribute('data-is-mobile') === 'true') {
+  var nonMobileExamples = document.querySelectorAll('[data-supports-mobile="false"]');
+  for (var i = 0; i < nonMobileExamples.length; i++) {
+    nonMobileExamples[i].parentNode.removeChild(nonMobileExamples[i]);
   }
-  return str;
 }
 
-var currentExample;
-var examplesSubnav = document.querySelector('#examplesSubnav');
-var examplesRoutes = {};
-
-function getUrlParameter (name) {
-  // Simple function because `URLSearchParams` isn't supported everywhere yet.
-  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-  var results = regex.exec(location.search);
-  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
-
-getJSON(settings.siteBaseUrl + '/examples/index.json', function (err, examples) {
-  if (err) {
-    console.error('Could not fetch examples\n%s', err);
-    return;
-  }
-
-  fetchExamples(examples);
-  init();
+// Hook up arrow keys.
+document.body.addEventListener('keyup', function (e) {
+  var currentLink;
+  var left = e.keyCode === 37;
+  var right = e.keyCode === 39;
+  if (!left && !right) { return; }
+  currentLink = getCurrentNavLink();
+  if (!currentLink) { return; }
+  clickNavLink(left ? getPrevNavLink() : getNextNavLink());
 });
 
-function fetchExamples (examples) {
-  if (examplesSubnav) {
-    examples.showcase.forEach(function (item, idx) {
-      item.getLi = function () {
-        return $('.example-item[data-slug="' + this.slug + '"]');
-      };
-      item.getA = function () {
-        return $('.example-item[data-slug="' + this.slug + '"] a');
-      };
-      item.isCurrent = item.example_url === pathWithSlash;
-      if (item.isCurrent) {
-        currentExample = item;
-      }
-      examplesRoutes[item.example_url] = item;
-    });
-  }
+// Hook up navigation arrows.
+var examplePrev = document.querySelector('#examplePrev');
+var exampleNext = document.querySelector('#exampleNext');
+examplePrev.addEventListener('click', function () {
+  window.location.href = getDestNavLink(true);
+});
+exampleNext.addEventListener('click', function () {
+  window.location.href = getDestNavLink(false);
+});
 
-  examplesRoutes.__default__ = examples.showcase[0];
-  examplesRoutes.__home__ = examples.home;
+// Inspector.
+var exampleIframe = document.querySelector('#exampleIframe');
+var exampleInspector = document.querySelector('#exampleInspector');
+var exampleViewsource = document.querySelector('#exampleViewSource');
+exampleInspector.addEventListener('click', function () {
+  // <ctrl> + <alt> + i.
+  exampleIframe.contentWindow.postMessage('INJECT_AFRAME_INSPECTOR', '*');
+  exampleInspector.style.display = 'none';
+  exampleViewsource.style.display = 'none';
+});
 
-  if (!currentExample) {
-    currentExample = settings.home ? examplesRoutes.__home__ : examplesRoutes.__default__;
-  }
+function clickNavLink (link) {
+  var currentLink = getCurrentNavLink();
+  if (!currentLink || currentLink === link) { return; }
+  currentLink.classList.remove('current');
+  currentLink.classList.remove('click');
+  // Click link.
+  link.classList.add('click');
+  link.click();
+  currentLink = link;
+  currentLink.classList.add('current');
+  currentLink.classList.remove('click');
 }
 
-function init () {
-  var body = document.body;
-
-  var currentLink;
-
-  // Trigger `:active` styles when we "click" on examples, previous/next links.
-  var SHOW_ACTIVE_STYLES_ON_CLICK = true;
-
-  function getTitle (title) {
-    return settings.title.replace('{title}', title);
-  }
-
-  function clickEl (el) {
-    if (!el) { return; }
-    if (SHOW_ACTIVE_STYLES_ON_CLICK) { el.classList.add('click'); }
-    el.click();
-  }
-
-  function clickNavLink (link) {
-    currentLink = getCurrentNavLink();
-    if (!currentLink || currentLink === link) { return; }
-    currentLink.classList.remove('current');
-    currentLink.classList.remove('click');
-    clickEl(link);
-    currentLink = link;
-    currentLink.classList.add('current');
-    currentLink.classList.remove('click');
-  }
-
-  function getCurrentNavLink () {
-    return document.querySelector('.examples-subnav .subnav-link.current');
-  }
-
-  function getNavLinks () {
-    return document.querySelectorAll('.examples-subnav .subnav-link');
-  }
-
-  function getDestNavLink (left) {
-    currentLink = getCurrentNavLink();
-    var navLinks = getNavLinks();
-    var navLinksLength = navLinks.length;
-
-    var idx = currentLink ? parseInt(currentLink.parentNode.getAttribute('data-idx'), 10) : -1;
-
-    var offset = left ? -1 : 1;
-
-    var nextIdx = idx + offset;
-    if (nextIdx < 0) { nextIdx = navLinksLength - 1; }
-
-    var destLink = navLinks[nextIdx % navLinksLength];
-
-    return destLink;
-  }
-
-  function getPrevNavLink () {
-    return getDestNavLink(true);
-  }
-
-  function getNextNavLink () {
-    return getDestNavLink(false);
-  }
-
-  body.addEventListener('keyup', function (e) {
-    var left = e.keyCode === 37;
-    var right = e.keyCode === 39;
-    if (!left && !right) { return; }
-
-    currentLink = getCurrentNavLink();
-
-    if (!currentLink) {
-      // TODO: Swap out when SPA is engaged.
-      window.location.href = settings.siteBaseUrl + '/examples/';
-      return;
-    }
-
-    var destLink = left ? getPrevNavLink() : getNextNavLink();
-    clickNavLink(destLink);
-  });
-
-  var examplePrev = document.querySelector('#examplePrev');
-  var exampleNext = document.querySelector('#exampleNext');
-  examplePrev.addEventListener('click', function () {
-    this.setAttribute('href', getPrevNavLink());
-  });
-  exampleNext.addEventListener('click', function () {
-    this.setAttribute('href', getNextNavLink());
-  });
-
-  // Inspector.
-  var exampleIframe = document.querySelector('#exampleIframe');
-  var exampleInspector = document.querySelector('#exampleInspector');
-  var exampleViewsource = document.querySelector('#exampleViewSource');
-  exampleInspector.addEventListener('click', function () {
-    // <ctrl> + <alt> + i.
-    exampleIframe.contentWindow.postMessage('INJECT_AFRAME_INSPECTOR', '*');
-    exampleInspector.style.display = 'none';
-    exampleViewsource.style.display = 'none';
-  });
-
-  if (settings.isSpa) {
-    var showPage = singlePage(function (href) {
-      var hrefWithSlash = forceTrailingSlash(parseUrl(href).pathname);
-
-      if (exampleIframe) {
-        if (!settings.isHome) {
-          document.title = getTitle(currentExample.title);
-          currentExample.getLi().classList.add('current');
-          currentExample.getA().classList.add('current');
-        }
-
-        exampleIframe.setAttribute('src', currentExample.scene_url);
-
-        if (exampleViewsource) {
-          exampleViewsource.setAttribute('href', currentExample.source_url);
-        }
-      }
-    });
-
-    body.addEventListener('spa-navigate', function (e) {
-      var link = $('[data-spa-link][href="' + e.detail.path + '"]');
-      if (link) {
-        clickNavLink(link);
-        showPage(e.detail.path);
-      }
-    });
-  }
+function getCurrentNavLink () {
+  return document.querySelector('.examples-subnav li[data-current="true"]');
 }
 
+function getDestNavLink (prev) {
+  var link = getCurrentNavLink()
+  var newLink = prev ? link.previousElementSibling : link.nextElementSibling;
+  if (!newLink) { newLink = document.querySelector('.examples-subnav > li:first-child'); }
+  return newLink.querySelector('a').getAttribute('href');
+}
+
+function getPrevNavLink () {
+  return getDestNavLink(true);
+}
+
+function getNextNavLink () {
+  return getDestNavLink(false);
+}
 })();
