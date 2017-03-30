16bff7623c35f7503717fe3ccb79efe7498950ef
Kevin Ngo <me@ngokevin.com>
2017-03-30 10:45:53 -0700

    add redirect for renamed guide

--- scripts/redirects.js
+++ scripts/redirects.js
@@ -78,7 +78,8 @@ hexo.extend.generator.register('docs-redirects', function () {
     ['docs/guide/objects.html', 'docs/0.2.0/guides/building-a-basic-scene.html'],
     ['docs/guide/positioning.html', 'docs/0.2.0/guides/building-a-basic-scene.html#transforming-the-box'],
     ['docs/primitives/a-cube.html', 'docs/0.2.0/primitives/a-box.html'],
-    ['docs/primitives/a-model.html', 'docs/0.2.0/primitives/a-collada-model.html']
+    ['docs/primitives/a-model.html', 'docs/0.2.0/primitives/a-collada-model.html'],
+    ['docs/0.5.0/building-with-components.html', 'docs/0.5.0/building-a-360-image-gallery.html']
   ]);
 
   // Flatten arrays since `redirectObjs` is an array of arrays of arrays. We just want a flat
