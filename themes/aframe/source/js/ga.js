8663a300f7e23bb49fc87dc3b72254e5f517ee5c
Kevin Ngo <me@ngokevin.com>
2017-07-18 16:08:27 -0700

    add original tracker on top of new tracker to not destroy history

--- themes/aframe/source/js/ga.js
+++ themes/aframe/source/js/ga.js
@@ -1,6 +1,7 @@
 (function () {
 
 function initGoogleAnalyticsEvents () {
+  window.ga('create', 'UA-24056643-4', 'auto', {name: 'originalTracker'});
   window.ga('send', 'event', 'pageload.querystring', window.location.search);
   window.ga('send', 'event', 'pageload.hash', window.location.hash);
   window.ga('send', 'event', 'supports.getVRDevices', 'getVRDevices' in navigator);
